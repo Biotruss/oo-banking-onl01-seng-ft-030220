bdb13eacc53572f3d4ba716d7ed0aae51c295a98
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 12:56:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
-  attr_reader :sender, :receiver, :transfer_amount, :status
+  attr_reader :sender, :receiver, :amount, :status
   
-  def initialize(sender, receiver, transfer_amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @transfer_amount = transfer_amount
