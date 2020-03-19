0ee7b62a98a7fe2cbc09177e172aeb7b7abc07c5
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 12:53:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    status = "pending"
+  end
 end
